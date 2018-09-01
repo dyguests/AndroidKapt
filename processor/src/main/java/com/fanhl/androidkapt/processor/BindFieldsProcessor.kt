72e08a6b18211a869d299ea74f76544924d8c8e1
fanhl <dyguests@qq.com>
2018-09-01 22:56:04 +0800

    Using your new Annotation Processor

--- processor/src/main/java/com/fanhl/androidkapt/processor/BindFieldsProcessor.kt
+++ processor/src/main/java/com/fanhl/androidkapt/processor/BindFieldsProcessor.kt
@@ -2,6 +2,7 @@ package com.fanhl.androidkapt.processor
 
 import com.fanhl.androidkapt.annotations.BindField
 import com.google.auto.service.AutoService
+import java.io.IOException
 import javax.annotation.processing.*
 import javax.lang.model.SourceVersion
 import javax.lang.model.element.ElementKind
@@ -19,8 +20,50 @@ class BindFieldsProcessor : AbstractProcessor() {
                 processingEnv.messager.printMessage(Diagnostic.Kind.ERROR, "Can only be applied to functions,  element: $methodElement ")
                 return false
             }
-//            ...
+            val generatedSourcesRoot: String = processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME].orEmpty()
+            if (generatedSourcesRoot.isEmpty()) {
+                processingEnv.messager.printMessage(Diagnostic.Kind.ERROR, "Can't find the target directory for generated Kotlin files.")
+                return false
+            }
         }
+
+        //last
+        val builder = StringBuilder()
+                .append("package com.fanhl.androidkapt.processor.generated;\n\n")
+                .append("public class GeneratedClass {\n\n") // open class
+                .append("\tpublic String getMessage() {\n") // open method
+                .append("\t\treturn \"")
+
+
+        // for each javax.lang.model.element.Element annotated with the CustomAnnotation
+        roundEnv?.getElementsAnnotatedWith(BindField::class.java)?.forEach { element ->
+            val objectType = element.getSimpleName().toString()
+
+
+            // this is appending to the return statement
+            builder.append(objectType).append(" says hello!\n")
+        }
+
+
+        builder.append("\"; \n ") // end return
+                .append("\t}\n") // close method
+                .append("}\n") // close class
+
+
+        try { // write the file
+            val source = processingEnv.filer.createSourceFile("com.fanhl.androidkapt.processor.generated.GeneratedClass")
+
+
+            val writer = source.openWriter()
+            writer.write(builder.toString())
+            writer.flush()
+            writer.close()
+        } catch (e: IOException) {
+            // Note: calling e.printStackTrace() will print IO errors
+            // that occur from the file already existing after its first run, this is normal
+        }
+
+
 
         return true
     }
